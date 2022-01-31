ea941be1d826d66ecdac321cbc40ea05bbfab603
Alicia Kmak <alicia.kmak@gmail.com>
2022-01-31 20:11:08 +0100

    add iteration 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import IdCard from './Components/IdCard';
 import Greetings from './Components/Greetings';
 import Random from './Components/Random';
+import BoxColor from './Components/BoxColor';
 
 function App() {
   return (
@@ -39,9 +40,12 @@ function App() {
       />
       <Greetings lang="de">Ludwig</Greetings>
       <Greetings lang="fr">François</Greetings>
-      
+
       <Random min={1} max={6} />
       <Random min={1} max={100} />
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
