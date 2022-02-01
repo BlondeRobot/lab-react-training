ff9327384537dda6e8fab9a1a8497c9921ea6e91
Alicia Kmak <alicia.kmak@gmail.com>
2022-02-01 20:17:23 +0100

    fixed iteration 7

--- src/Components/DriverCard.jsx
+++ src/Components/DriverCard.jsx
@@ -1,17 +1,18 @@
+function DriverCard({ name, rating, img, car }) {
+  let roundedRating = Math.round(Number(rating));
+  console.log(roundedRating);
+  rating = '★'.repeat(roundedRating) + '☆'.repeat(5 - roundedRating);
 
-function DriverCard({name, rating, img, car}) {
-        let roundedRating = Math.round(Number({rating}));
-        rating = '★'.repeat(roundedRating) + '☆'.repeat(5 - roundedRating);
-    return (
-      <div>
-        <img src={img} alt="driver" style={{ height: '50px' }} />
-        <p>{name}</p>
-        <p>{rating}</p>
-        <p>
-          {car.model}-{car.licensePlate}
-        </p>
-      </div>
-    );
+  return (
+    <div>
+      <img src={img} alt="driver" style={{ height: '50px' }} />
+      <p>{name}</p>
+      <p>{rating}</p>
+      <p>
+        {car.model}-{car.licensePlate}
+      </p>
+    </div>
+  );
 }
 
-export default DriverCard
+export default DriverCard;
